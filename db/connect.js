99dc5d9f39b0bb30c3f06391b67cc5319d553004
harshith <harshithkalki@gmail.com>
2022-07-12 22:57:55 +0530

    removed database url

--- db/connect.js
+++ db/connect.js
@@ -1,11 +1,14 @@
 const mongoose=require('mongoose');
 
 
-const dburl='mongodb://localhost:27017/taskmanager';
-mongoose.connect(dburl)
-.then(()=>{
-    console.log("CONNECTED TO THE DB...");
-})
-.catch((err)=>{
-    console.log(err)
-})
+
+const connectDB=(url)=>{
+    return mongoose.connect(url,{
+        useNewUrlParser:true,
+        useCreateIndex:true,
+        useFindAndModify:false,
+        useUnifiedTopology:true
+    })
+}
+
+module.exports=connectDB;
