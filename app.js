47dea75eef19a573c51a3fb1eb587957ad79894a
harshith <harshithkalki@gmail.com>
2022-07-13 23:19:16 +0530

    app updated

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 const express = require('express');
 const tasks=require('./routes/tasks');
 const connectDB=require('./db/connect');
-const Task= require('./models/Task');
 require('dotenv').config();
 //setup
 const app=express();
