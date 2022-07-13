fb2cb375f1298ad0c82783a0fc6fb4c827069ac8
harshith <harshithkalki@gmail.com>
2022-07-13 23:20:17 +0530

    added all the api functions

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,25 +1,67 @@
 const Task= require('../models/Task');
 
 
-const getAllTasks=(req,res)=>{
-    res.send("all filessss");
+const getAllTasks=async (req,res)=>{
+    try{
+        const data=await Task.find({});
+        res.status(200).json({tasks:data});
+    }catch(error){
+        res.status(500).json({msg:error});
+    }
+    // res.send("all filessss");
 }
 
 const createTask=async (req,res)=>{
-    const task= await Task.create(req.body);
-    res.status(201).json({task});
+    try{
+        const task= await Task.create(req.body);
+        res.status(201).json({task});
+    }catch(error){
+        res.status(500).json({msg:error});
+    }
+
 }
 
-const getTask=(req,res)=>{
-    res.json({id:req.params.id});
+const getTask=async(req,res)=>{
+    // res.json({id:req.params.id});
+    try{
+        const id=req.params.id;
+        const task=await Task.findById(id);
+        // const task=await Task.findOne({_id:id});
+        if(!task){
+            return res.status(404).json({msg:"no task with that Id"})
+        }
+        res.status(200).json({task});
+    }catch(error){
+        res.status(500).json({msg:error});
+    }
 }
 
-const updateTask=(req,res)=>{
-    res.json({id:req.params.id,"method":req.method});
+const updateTask=async (req,res)=>{
+    try {
+        const id=req.params.id;
+        const task=await Task.findOneAndUpdate({_id:id},req.body,{new:true,
+        runValidators:true});
+        if(!task){
+            return res.status(404).json({msg:"no task with that Id"})
+        }
+        res.status(200).json({task});
+    } catch (error) {
+        
+    }
 }
 
-const deleteTask=(req,res)=>{
-    res.json({id:req.params.id,"method":req.method});
+const deleteTask=async (req,res)=>{
+    try{
+        const id=req.params.id;
+        const task=await Task.findOneAndDelete({_id:id});
+        // const task=await Task.findOne({_id:id});
+        if(!task){
+            return res.status(404).json({msg:"no task with that Id"})
+        }
+        res.status(200).json({status:'ok'});
+    }catch(error){
+        res.status(500).json({msg:error});
+    }
 }
 
 
