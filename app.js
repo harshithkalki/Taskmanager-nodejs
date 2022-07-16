6374a83aea18a76f945b98f126b91cdbfe9e131f
harshith <harshithkalki@gmail.com>
2022-07-16 16:25:26 +0530

    imported and used new middleware

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const tasks=require('./routes/tasks');
 const connectDB=require('./db/connect');
 const notFound=require('./middleware/not-found');
+const errorHandlerMiddleware= require('./middleware/error-handler');
 require('dotenv').config();
 //setup
 const app=express();
@@ -22,7 +23,9 @@ start();
 app.use(express.static('public'));
 app.use(express.json());
 
+
 //routes
 app.use('/api/v1/tasks',tasks)
 
 app.use(notFound)
+app.use(errorHandlerMiddleware);
